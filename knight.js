e1d6dbbe3b25ad7932f1320d14387058371000f9
Haseeb <ah085904@gmail.com>
2026-09-25 15:46:48 +0500

    fix: get all 8 moves instead of 4

--- knight.js
+++ knight.js
@@ -2,8 +2,23 @@ function pushCoordinates(list, x, y) {
   list.push([x, y]);
 }
 
-function knightMoves(x, y) {
+function doesCoordinatesExist(list, x, y, index = 0) {
+  doesCoordinatesExist(list[index], x, y, index + 1);
+
+  if (list.includes(x) && list.includes(y)) {
+    return true;
+  }
+  if (index > list.length - 1) {
+    return false;
+  }
+}
+function knightMoves(start, end) {
   let list = [];
+
+  let [x, y] = start;
+
+  let [x1, y1] = end;
+
   if (x + 1 < 7 && y + 2 < 7) {
     pushCoordinates(list, x + 1, y + 2);
   }
@@ -19,7 +34,25 @@ function knightMoves(x, y) {
     pushCoordinates(list, x - 2, y - 1);
   }
 
-  return list;
+  if (x - 2 >= 0 && y + 1 < 7) {
+    pushCoordinates(list, x - 2, y + 1);
+  }
+
+  if (x + 1 < 7 && y - 2 >= 0) {
+    pushCoordinates(list, x + 1, y - 2);
+  }
+
+  if (x + 2 < 7 && y - 1 >= 0) {
+    pushCoordinates(list, x + 2, y - 1);
+  }
+
+  if (x - 1 >= 0 && y + 2 < 7) {
+    pushCoordinates(list, x - 1, y + 2);
+  }
+
+  return list.sort((a, b) => a - b);
 }
 
-console.log(knightMoves(7, 7));
+console.log(knightMoves([3, 3], [4, 3]));
+
+// [1,2], [2,1], [4,1], [5,2], [5,4], [4,5], [2,5], [1,4]
