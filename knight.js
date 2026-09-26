2f90ce7ffb8760b77e62b11323f25e694ad331f2
Haseeb <ah085904@gmail.com>
2026-09-26 17:17:59 +0500

    fix: include 7 as a coordinate too

--- knight.js
+++ knight.js
@@ -1,11 +1,11 @@
 function getAllPossibleMoves(x, y) {
   const moves = [];
 
-  if (x + 1 < 7 && y + 2 < 7) {
+  if (x + 1 < 7 && y + 2 <= 7) {
     moves.push([x + 1, y + 2]);
   }
 
-  if (x + 2 < 7 && y + 1 < 7) {
+  if (x + 2 < 7 && y + 1 <= 7) {
     moves.push([x + 2, y + 1]);
   }
   if (x - 1 >= 0 && y - 2 >= 0) {
@@ -16,19 +16,19 @@ function getAllPossibleMoves(x, y) {
     moves.push([x - 2, y - 1]);
   }
 
-  if (x - 2 >= 0 && y + 1 < 7) {
+  if (x - 2 >= 0 && y + 1 <= 7) {
     moves.push([x - 2, y + 1]);
   }
 
-  if (x + 1 < 7 && y - 2 >= 0) {
+  if (x + 1 <= 7 && y - 2 >= 0) {
     moves.push([x + 1, y - 2]);
   }
 
-  if (x + 2 < 7 && y - 1 >= 0) {
+  if (x + 2 <= 7 && y - 1 >= 0) {
     moves.push([x + 2, y - 1]);
   }
 
-  if (x - 1 >= 0 && y + 2 < 7) {
+  if (x - 1 >= 0 && y + 2 <= 7) {
     moves.push([x - 1, y + 2]);
   }
 
@@ -36,23 +36,12 @@ function getAllPossibleMoves(x, y) {
 }
 
 function knightMoves(start, end) {
-  let list = [];
-
+  if (!start) return;
   let [x, y] = start;
 
   let [x1, y1] = end;
 
-  return getAllPossibleMoves(x, y);
+  if (x === x1 && y === y1) return true;
 }
 
-let list = knightMoves([0, 0], [4, 3]);
-
-function doesCoordinatesExist(x, y) {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i][0] === x && list[i][1] === y) {
-      return true;
-    }
-  }
-
-  return false;
-}
+console.log(knightMoves([3, 3], [4, 3]));
